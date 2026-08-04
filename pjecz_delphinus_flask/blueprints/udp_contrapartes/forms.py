ccc125da07679230d422888f7f8a3267b2cda806
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2026-08-04 12:28:27 -0600

    05 formulario contraparte

---
+++ pjecz_delphinus_flask/blueprints/udp_contrapartes/forms.py
@@ -0,0 +1,19 @@
+"""
+UDP Contrapartes, formularios
+"""
+
+from flask_wtf import FlaskForm
+from wtforms import DateField, StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length, Optional
+
+
+class UdpContraparteForm(FlaskForm):
+    """Formulario UdpContraparte"""
+
+    nombres = StringField("Nombres", validators=[DataRequired(), Length(max=256)])
+    apellido_primero = StringField("Apellido Primero", validators=[DataRequired(), Length(max=256)])
+    apellido_segundo = StringField("Apellido Segundo", validators=[Optional(), Length(max=256)])
+    curp = StringField("CURP", validators=[Optional(), Length(max=18)])
+    nacimiento_fecha = DateField("Fecha de Nacimiento", validators=[Optional()])
+    observaciones = TextAreaField("Observaciones", validators=[Optional(), Length(max=1024)])
+    guardar = SubmitField("Guardar")
